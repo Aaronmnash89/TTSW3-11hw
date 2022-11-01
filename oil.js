eaa70c3caf55a0308c42d03c2479075b02bd0039
Aaron Nash <aaronmnash89@gmail.com>
2022-11-01 11:39:39 -0400

    Add files via upload

---
+++ oil.js
@@ -0,0 +1,9 @@
+let milage = 600;
+if(milage => 500) {
+    console.log("Yes I think I need an oil change.");
+} else if (milage <= 500) {
+    console.log("My car doesn't need an oil change yet.");
+};
+
+
+console.log(milage)
