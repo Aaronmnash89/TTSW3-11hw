69f4a736d19e08a2f73f34645b5055b487403a21
Aaron Nash <aaronmnash89@gmail.com>
2022-11-02 10:02:04 -0400

    homework for week 3

---
+++ week3/day3/practice.js
@@ -0,0 +1,9 @@
+let cups = 5;
+if(cups <= 3) {
+    console.log("Yes I'll take another cup of coffee.");
+} else if (cups => 3) {
+    console.log("I think I'm okay for now.");
+};
+
+
+console.log(cups)
